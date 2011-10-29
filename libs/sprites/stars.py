d5620cc8abda7effa4edb19023abf3b1f01e4097
Daniel Espino <danito@gumersindo.(none)>
2011-10-29 22:51:50 +0200

    Some fixes, but not working the Select widgets

--- libs/sprites/stars.py
+++ libs/sprites/stars.py
@@ -1,8 +1,14 @@
 from galaxdustk.base import Widget
+import pygame
+
+def _go_to_star(widget):
+    widget.screen.context.active_screen = "star"
+    widget.screen.context.screens['star'].active_star = widget.star
 
 class SmallStar(Widget):
     def __init__(self, screen, star):
-        super(StarSprite,self).__init__(screen)
+        super(SmallStar,self).__init__(screen)
         self.star = star
         self.image = pygame.image.load('data/images/stars/star_%d.png' % star.star_type)
         self.rect = self.image.get_rect()
+	self.connect('clicked', _go_to_star)
